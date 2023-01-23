effd2f3c248c5e01de19e621d43bb2f9d16fd991
Simon-2022 <96212730+Simon-2022@users.noreply.github.com>
2023-01-23 19:34:15 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var difference = 0;
+var rightWristX = 0;
+var leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -8,14 +12,28 @@ function setup() {
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-function modelLoaded() {
-    console.log('PoseNet Is Initialized');
-}
-function draw() {
-    background('#969A97');
-}
+
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    background('#969A97');
+    textSize(difference);
+    document.getElementById("square_sides").innerHTML = "Width And Height of the font will be = " + difference + "px";
+    fill('blue');
+    text("Simon", 150, 250)
+    stroke('red');
 }
